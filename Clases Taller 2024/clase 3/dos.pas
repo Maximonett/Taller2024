79859c5e11f6dc495eb5e32ce6c182a6b8c700cd
Maximonett <maximosimonetti@gmail.com>
2024-08-27 13:33:00 -0300

    up

--- Clases Taller 2024/clase 3/dos.pas
+++ Clases Taller 2024/clase 3/dos.pas
@@ -20,129 +20,256 @@ con mayor cantidad de ventas.}
 
 program dos;
 
-type 
-
-    venta=record
-        codP:integer;
-        fecha:string;
-        cantU:integer;
+type
+    venta = record
+        codP: integer;
+        fecha: string;
+        cantU: integer;
     end;
 
-    arbolV=^nodo;
-    nodo=record
-        dato:venta;
-        HI:arbolV;
-        HD:arbolV;
-    end;
-        
-    producto=record
-        codP:integer;
-        cantTotalV:integer;
+    arbolV = ^nodo;
+    nodo = record
+        dato: venta;
+        HI: arbolV;
+        HD: arbolV;
     end;
 
-    arbolP=^nodoP;
-    nodoP=record
-        dato:producto;
-        HI:arbolP;
-        Hd:arbolP;
+    producto = record
+        codP: integer;
+        cantTotalV: integer;
     end;
 
+    arbolP = ^nodoP;
+    nodoP = record
+        dato: producto;
+        HI: arbolP;
+        HD: arbolP;
+    end;
 
-procedure GenerarArbol(var aV:arbolV);
+    listaV = ^nodoV;
+    nodoV = record
+        dato: venta;
+        sig: listaV;
+    end;
 
-    procedure cargarVentas(var v:venta);
+    arbolLV = ^nodoLV;
+    nodoLV = record
+        codigoP: integer;
+        ventas: listaV;  // Este campo almacenará la lista de ventas para un producto.
+        HI: arbolLV;
+        HD: arbolLV;
+    end;
+
+procedure GenerarArbol(var aV: arbolV);
+
+    procedure cargarVentas(var v: venta);
     var
-        vFechas:array[0..9]of String=('1 de marzo','3 de octubre','10 de enero','11 de agosto','3 de noviembre', '2 de junio','22 de febrero', '6 de julio','7 de enero','21 de diciembre');
+        vFechas: array[0..9] of String = ('1 de marzo', '3 de octubre', '10 de enero', '11 de agosto', '3 de noviembre', '2 de junio', '22 de febrero', '6 de julio', '7 de enero', '21 de diciembre');
     begin
-        v.codP:=random(51)*100;
-        if (v.codP<>0) then
-            v.fecha:=vFechas[random(10)];
-            v.cantU:=random(100);
+        v.codP := random(51) * 100;
+        if (v.codP <> 0) then begin
+            v.fecha := vFechas[random(10)];
+            v.cantU := random(100);
+        end;
     end;
 
-    procedure InsertarElemento(var aV:arbolV; elem:venta);
+    procedure InsertarElemento(var aV: arbolV; elem: venta);
     begin
-        if (aV=nil) then begin
+        if (aV = nil) then begin
             new(aV);
-            aV^.dato:=elem;
-            aV^.HI:=nil;
-            aV^.HD:=nil;
+            aV^.dato := elem;
+            aV^.HI := nil;
+            aV^.HD := nil;
         end
-        else 
-            if (elem.codP<aV^.dato.codP) then
-                InsertarElemento(aV^.HI,elem)
-            else 
-                InsertarElemento(aV^.HD,elem);
+        else if (elem.codP < aV^.dato.codP) then
+            InsertarElemento(aV^.HI, elem)
+        else
+            InsertarElemento(aV^.HD, elem);
     end;
 
-var 
-    unaVenta:venta;
+var
+    unaVenta: venta;
 begin
-    aV:=nil;
     WriteLn('---Generando Arbol de Ventas----');
     cargarVentas(unaVenta);
-    while (unaVenta.codP<>0) do begin
-        InsertarElemento(aV,unaVenta);
+    while (unaVenta.codP <> 0) do begin
+        InsertarElemento(aV, unaVenta);
         cargarVentas(unaVenta);
     end;
 end;
 
-procedure mostrarArbol(a:arbolV);
+procedure mostrarArbol(a: arbolV);
 begin
-    if (a<> nil)then begin
+    if (a <> nil) then begin
         mostrarArbol(a^.HI);
-        WriteLn('** Codigo de Producto: ',a^.dato.codP,'--**Fecha: ', a^.dato.fecha,'--**Cantidad de  Unidades Vendidas: ',a^.dato.cantU,' **');
+        WriteLn('** Codigo de Producto: ', a^.dato.codP, '--**Fecha: ', a^.dato.fecha, '--**Cantidad de  Unidades Vendidas: ', a^.dato.cantU, ' **');
         mostrarArbol(a^.HD);
     end;
 end;
 
-function sumar(a:arbolV):integer;
+procedure actualizarOInsertarProducto(var aP: arbolP; codP: integer; cantU: integer);
 begin
-    if (a=nil) then
-        sumar:=0
+    if (aP = nil) then begin  
+        new(aP);
+        aP^.dato.codP := codP;
+        aP^.dato.cantTotalV := cantU;
+        aP^.HI := nil;
+        aP^.HD := nil;
+    end
+    else if (codP < aP^.dato.codP) then   
+        actualizarOInsertarProducto(aP^.HI, codP, cantU)
+    else if (codP > aP^.dato.codP) then
+        actualizarOInsertarProducto(aP^.HD, codP, cantU)
     else
-        sumar:=a^.dato.cantU +sumar(a^.HI)+ sumar(a^.HD);
+        aP^.dato.cantTotalV := aP^.dato.cantTotalV + cantU;  // Si ya existe el producto, sumamos la cantidad
 end;
 
-procedure InsertarElementoP(var aP:arbolP; p:producto);
-    begin
-        if (aP=nil) then begin
-            new(aP);
-            aP^.dato:=p;
-            aP^.HI:=nil;
-            aP^.HD:=nil;
-        end
-        else 
-            if (p.codP<aP^.dato.codP) then
-                InsertarElementoP(aP^.HI,p)
-            else 
-                InsertarElementoP(aP^.HD,p);
-    end;
-
-{ii. Generar y retornar otro árbol binario de búsqueda de productos 
-vendidos ordenado por código de producto. 
-Cada nodo del árbol debe contener el código de producto y la
-cantidad total de unidades vendidas.}
-
-procedure generarArbolP(aV:arbolV;var aP:arbolP);
-var
-    p:producto;
+procedure generarArbolP(aV: arbolV; var aP: arbolP);
 begin
-    p.codP:=aV^.dato.codP;
-    if (p.codP<>0) then begin
-        p.cantTotalV:=sumar(aV);
-        InsertarElementoP(aP,p);
-    end; 
+    if aV <> nil then begin
+        actualizarOInsertarProducto(aP, aV^.dato.codP, aV^.dato.cantU);
+        generarArbolP(aV^.HI, aP);
+        generarArbolP(aV^.HD, aP);
+    end;
 end;
 
-procedure mostrarArbolP(a:arbolP);
-var 
-    a:arbolV;
-    aP:arbolP;
+procedure mostrarArbolP(a: arbolP);
+begin
+    if (a <> nil) then begin
+        mostrarArbolP(a^.HI);
+        WriteLn('El codigo de producto es: ', a^.dato.codP);
+        WriteLn('La cantidad de Unidades Totales Vendidas es: ', a^.dato.cantTotalV);
+        mostrarArbolP(a^.HD);
+    end;
+end;
+
+procedure agregarVentaLista(var L: listaV; v: venta);
+var
+    nuevo: listaV;
+begin
+    new(nuevo);
+    nuevo^.dato := v;
+    nuevo^.sig := L;
+    L := nuevo;
+end;
+
+procedure actualizarOInsertarArbolLV(var aLV: arbolLV; v: venta);
+begin
+    if (aLV = nil) then begin
+        new(aLV);
+        aLV^.codigoP := v.codP;
+        aLV^.ventas := nil;
+        agregarVentaLista(aLV^.ventas, v);
+        aLV^.HI := nil;
+        aLV^.HD := nil;
+    end
+    else if (v.codP < aLV^.codigoP) then
+        actualizarOInsertarArbolLV(aLV^.HI, v)
+    else if (v.codP > aLV^.codigoP) then
+        actualizarOInsertarArbolLV(aLV^.HD, v)
+    else
+        agregarVentaLista(aLV^.ventas, v);  // Si ya existe el producto, añadimos la venta a la lista.
+end;
+
+procedure generarArbolLV(aV: arbolV; var aLV: arbolLV);
+begin
+    if aV <> nil then begin
+        actualizarOInsertarArbolLV(aLV, aV^.dato);
+        generarArbolLV(aV^.HI, aLV);
+        generarArbolLV(aV^.HD, aLV);
+    end;
+end;
+
+procedure mostrarListaVentas(L: listaV);
+begin
+    while L <> nil do begin
+        WriteLn('   Venta - Fecha: ', L^.dato.fecha, ', Cantidad: ', L^.dato.cantU);
+        L := L^.sig;
+    end;
+end;
+
+procedure mostrarArbolLV(a: arbolLV);
+begin
+    if (a <> nil) then begin
+        mostrarArbolLV(a^.HI);
+        WriteLn('Código de Producto: ', a^.codigoP);
+        mostrarListaVentas(a^.ventas);
+        mostrarArbolLV(a^.HD);
+    end;
+end;
+{b. Implemente un módulo que reciba el árbol generado en i. 
+y una fecha y retorne la cantidad
+total de productos vendidos en la fecha recibida.}
+
+function cantidadProductos(a:arbolV; fecha:string):integer;
+begin
+    if(a=nil) then
+        cantidadProductos:=0
+    else begin
+        if (a^.dato.fecha=fecha)then
+            cantidadProductos:= a^.dato.cantU + cantidadProductos(a^.HI,fecha) 
+                                            + cantidadProductos(a^.HD,fecha)
+        else cantidadProductos:= cantidadProductos(a^.HI,fecha) 
+                                + cantidadProductos(a^.HD,fecha)
+    end;
+end;
+
+{c. Implemente un módulo que reciba el árbol generado en ii. 
+y retorne el código de producto con mayor cantidad 
+total de unidades vendidas.}
+
+function codigoProductoMasVendido(aP:arbolP):integer;
+var
+    maxCodigo:integer;
+    maxCantidad:integer;
+
+    procedure buscarMaximo(aP:arbolP);
+    begin
+        if (aP<>nil) then begin
+            if(aP^.dato.cantTotalV>maxCantidad)then begin
+                maxCantidad:=aP^.dato.cantTotalV;
+                maxCodigo:=aP^.dato.codP;
+            end;
+            buscarMaximo(aP^.HI);
+            buscarMaximo(aP^.HD);
+    end;
+begin
+    maxCantidad := -1;  // Inicia con un valor bajo
+    maxCodigo := -1;    // Inicia con un código no válido
+    buscarMaximo(aP);   // Llama a la función recursiva para encontrar el máximo
+    codigoProductoMasVendido := maxCodigo;  // Retorna el código con más ventas
+end;
+
+
+var
+    a: arbolV;
+    aP: arbolP;
+    aLV:arbolLV;
+    fecha:String;
 begin
     randomize();
+    a:=nil;
+    aP := nil;
     GenerarArbol(a);
+    WriteLn('+++++++++++++++++++++++++++++++');
+    WriteLn('++++++Arbol del Ventas++++++');
+    WriteLn('+++++++++++++++++++++++++++++++');
     mostrarArbol(a);
-    generarArbolP(a,aP)
+
+    WriteLn('+++++++++++++++++++++++++++++++');
+    WriteLn('++++++Arbol del Productos++++++');
+    WriteLn('+++++++++++++++++++++++++++++++');   
+    generarArbolP(a, aP);
     mostrarArbolP(aP);
+    WriteLn('+++++++++++++++++++++++++++++++++++++');
+    WriteLn('++++++Arbol de Listas de Ventas++++++');
+    WriteLn('+++++++++++++++++++++++++++++++++++++');
+    aLV:=Nil;
+    generarArbolLV(a,aLV);
+    mostrarArbolLV(aLV);
+    WriteLn();
+    WriteLn('Elija una fecha para ver la cantidad de productos vendidos ese dia: ');
+    ReadLn(fecha);
+    WriteLn('La cantidad de productos vendidos el dia ', fecha,' fue ', cantidadProductos(a,fecha),' productos.');
+    
 end.
